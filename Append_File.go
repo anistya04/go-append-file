a9066461ad4b8d9a95833399e0b12968f5824ef0
Anistya Dwi Setiawan <45922350+anistya04@users.noreply.github.com>
2024-01-13 10:13:42 +0700

    Update Append_File.go

--- Append_File.go
+++ Append_File.go
@@ -2,7 +2,7 @@ package goappendfile
 
 import "os"
 
-func appendFile(name string, message string) error {
+func AppendFile(name string, message string) error {
 	file, err := os.OpenFile(name, os.O_RDWR|os.O_APPEND, 0644)
 	if err != nil {
 		return err
